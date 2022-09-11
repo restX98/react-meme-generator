6a1bd357bf29aaec18bcd119fce0db7b3f896f5a
Enrico Restuccia <enrico.rest98@gmail.com>
2022-09-11 17:33:01 +0200

    Meme Generator: Update state object

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import memesData from "../memesData";
 import "./Meme.css";
 
 function Meme() {
+  const [allMemeImages] = useState(memesData);
+  const [meme, setMemeImage] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+
   function getMemeImage(e) {
     e.preventDefault();
-    const memesArray = memesData.data.memes;
-    console.log(memesArray[Math.floor(Math.random() * memesArray.length)].url);
+    const memesArray = allMemeImages.data.memes;
+    setMemeImage((prevMeme) => ({
+      ...prevMeme,
+      randomImage:
+        memesArray[Math.floor(Math.random() * memesArray.length)].url,
+    }));
   }
 
   return (
@@ -30,6 +42,9 @@ function Meme() {
           value="Get a new meme image"
         />
       </form>
+      <div className="meme-container">
+        {meme.randomImage && <img src={meme.randomImage} alt="Meme" />}
+      </div>
     </main>
   );
 }

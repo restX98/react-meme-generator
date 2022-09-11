d61770a84956f5586a5d9f82d9804994fe007d19
Enrico Restuccia <enrico.rest98@gmail.com>
2022-09-11 16:28:16 +0200

    Counter Example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Header from "./components/Header";
-import Meme from "./components/Meme";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <Header />
-      <Meme />
+    <div className="counter">
+      <button
+        className="counter--minus"
+        onClick={(e) => setCounter((prevCounter) => prevCounter - 1)}
+      >
+        –
+      </button>
+      <div className="counter--count">
+        <h1>{counter}</h1>
+      </div>
+      <button
+        className="counter--plus"
+        onClick={(e) => setCounter((prevCounter) => prevCounter + 1)}
+      >
+        +
+      </button>
     </div>
   );
 }
